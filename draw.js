1140027b31f13c1a7efab082be0ae3750effd7fd
reng <rengah0@gmail.com>
2023-07-28 18:09:01 +0800

    update

--- draw.js
+++ draw.js
@@ -1,6 +1,6 @@
 const __demo_text="公寓大門外，供桌擺得齊，爸爸在旁邊，看著我燒紙錢。特價傳單，家樂福的獻禮，微涼的晚風，帶走了我們的祈禱。煙火升起，照亮了這晚夜，我們在這裡，為中元節祈福。家樂福特價，供桌上的禮物，希望他們喜歡，這些我們的小心意。微涼的晚風，吹散了煙火，爸爸在旁邊，默默地看著我。供桌擺在公寓大門外，我們在這裡，為中元節祈福。";
 const __font="sans-serif";
-
+const __seperator=["，","。",",","."];
 
 function loadPixels(img, width, height){
 
@@ -16,9 +16,10 @@ function loadPixels(img, width, height){
 
 
 }
-function getContour(pixels, width, height, lineHeight){
+function getContour(pixels, width, height, count){
 
-    let count=Math.floor(height/lineHeight);
+    let lineHeight=Math.floor(height/count);
+    // let count=Math.floor(height/lineHeight);
     let output=[];
     
     for(var i=0;i<count;++i){
@@ -31,17 +32,24 @@ function getContour(pixels, width, height, lineHeight){
 
         output.push(start);
     }
-    // console.log(output);
+    console.log(output);
     return output;
 }
 
+function seperate(text){
+    let output=text;
+    __seperator.forEach(el=>{
+        output=output.split(el).join(`${el}|`);
+    })
+    return output.split("|");
+}
 
 function init(){
 
     // set defulat text
     document.getElementById("_input_text").value=__demo_text;
     
-    const _param_keys=['textSize', 'lineHeight','speed', 'color1', 'color2','text', 'direction'];
+    const _param_keys=['spacing','speed', 'color1', 'color2','text', 'direction'];
 
     let find=document.getElementsByTagName("canvas");
     if(find.length==0){
@@ -56,7 +64,7 @@ function init(){
     const image = new Image();
     image.onload = drawText;
 
-    let id=Math.floor(Math.random()*2)+1;
+    let id=2;//Math.floor(Math.random()*2)+1;
     image.src = `resources/face${id}.svg`;
     
     
@@ -72,8 +80,13 @@ function init(){
     //text=lines.join("，\n");
 
     
-    // let contour;
+    let contour;
+    let textSize;
+    let lineHeight;
+        
     let pixels;
+    let prev_params;
+    let prev_text;
 
     function drawText(){
 
@@ -81,7 +94,11 @@ function init(){
         let params={};
         _param_keys.forEach(key=>{
             let val=document.getElementById(`_input_${key}`).value;
-            params[key]=parseFloat(val) || val;
+            if(val=="checked"){
+                params[key]=document.getElementById(`_input_${key}`).checked;
+            }else{
+                params[key]=parseFloat(val) || val;
+            }
         });
 
 
@@ -93,18 +110,28 @@ function init(){
         if(!pixels){
             pixels = loadPixels(image, canvas.width, canvas.height);
         }
-        let contour=getContour(pixels, canvas.width, canvas.height, params.lineHeight);
-    
-       
+
+        let lines=seperate(text);
+        
+        if(!prev_params || prev_params.spacing!=params.spacing || prev_params.text != params.text){
+            contour=getContour(pixels, canvas.width, canvas.height, lines.length);            
+            lineHeight=canvas.height/lines.length;
+            textSize=lineHeight-params.spacing*2;
+        }
+        prev_params=JSON.parse(JSON.stringify(params));
+        
+        
+        
+        
         ctx.clearRect(0,0, canvas.width, canvas.height);
         // ctx.drawImage(image,0,0,image.width/image.height*canvas.height, canvas.height);
-
-        ctx.font = `${params.textSize}px ${__font}`;
+        
+        ctx.font = `${textSize}px ${__font}`;
 
         let p=(new Date()).getTime()/5000*params.speed;
         let pc=Math.abs(Math.sin(p/2.0));
 
-        var gradient=params.direction=="radial"? ctx.createRadialGradient(0,canvas.height/2,0,0,canvas.height/2,canvas.width): ctx.createLinearGradient(0,0,0,canvas.height+params.lineHeight);
+        var gradient=params.direction=="radial"? ctx.createRadialGradient(0,canvas.height/2,0,0,canvas.height/2,canvas.width): ctx.createLinearGradient(0,0,0,canvas.height+lineHeight);
         gradient.addColorStop('0', params.color2);
         gradient.addColorStop(pc, params.color1);
         gradient.addColorStop('1.0', params.color2);
@@ -112,23 +139,35 @@ function init(){
         ctx.fillStyle=gradient;
         
         let start=0;
+        // let lines;
+        // let breakline=(params.breakline);
+
+        // if(breakline){
+        // }
 
         for(var index=0; index < contour.length; index++){
 
-            if(start>=text.length) start=0;
+            let trim="";
+            // if(!breakline){
+            //     if(start>=text.length) start=0;
 
-            // let line=lines[index%lines.length];
-            // let wid=len*1.8;
-            let wid=Math.floor((canvas.width-contour[index])/params.textSize/1.2);
-            let sub=text.substring(start, start+wid);
-            let cut=Math.max(sub.lastIndexOf("，"), sub.lastIndexOf("。"));
-            let trim=sub.substring(0,cut+1);
-            start+=trim.length;
+            //     // let line=lines[index%lines.length];
+            //     // let wid=len*1.8;
+            //     let wid=Math.floor((canvas.width-contour[index])/textSize/1.2);
+            //     let sub=text.substring(start, start+wid);
+            //     let cut=Math.max(sub.lastIndexOf("，"), sub.lastIndexOf("。"));
+            //     trim=sub.substring(0,cut+1);
+            //     start+=trim.length;
 
+            // }else{
+                if(index>=lines.length) break;
+                trim=lines[index];
+            // }
+            let spacing=Math.min((canvas.width-contour[index])/trim.length, textSize*2.0);
             for(var i=0;i<trim.length;++i){
                 let char=trim.charAt(i);
-                let pp=0.4+0.8*Math.abs(Math.sin(index/contour.length*Math.PI+p));
-                ctx.fillText(char,contour[index]+i*params.textSize*pp, (index+1)*params.lineHeight); 
+                let pp=0.4+(0.8)*Math.abs(Math.sin(index/contour.length*Math.PI+p));
+                ctx.fillText(char,contour[index]+i*spacing*pp, (index+1)*lineHeight); 
 
                 // start++;
 
